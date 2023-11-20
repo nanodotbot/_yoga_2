f0764e099b23bd8ba04195c286032bacb34a7da6
nanodotbot <nanodotbot@gmail.com>
2023-11-20 20:20:42 +0100

    deleted

--- tell-me-something.php
+++
@@ -1,8 +0,0 @@
-<?php 
-// TODO: add to productive
-$ip = $_SERVER['REMOTE_ADDR']	;
-$mail_message = wordwrap($ip, 70);
-mail('info@nano.sx', 'angi.yoga', $mail_message);
-
-echo '{"message" : "delivered"}';
-?>
